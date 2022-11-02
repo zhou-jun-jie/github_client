07e394cba9c921ca5616beeea602eef66acb1ecc
zjj <1282450075@qq.com>
2022-11-02 23:14:50 +0800

    添加sp

---
+++ lib/common/Global.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:github_client/models/index.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:dio/dio.dart';
+
+const _themes = <MaterialColor>[
+  Colors.blue,
+  Colors.cyan,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+];
+
+class Global {
+  static late SharedPreferences _prefs;
+  static Profile profile = Profile();
+  // TODO 包疑问：网络缓存对象
+  // static NetCache netCache = NetCache();
+  
+  // TODO 写法疑问：可选的主题列表
+  static List<MaterialColor> get themes =>_themes;
+  
+  // 是否为release版本
+  static bool get isRelease => bool.fromEnvironment("dart.vm.product");
+
+  // 初始化全局信息,会在app启动时执行
+  static Future init() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    _prefs = await SharedPreferences.getInstance();
+    var _profile = _prefs.getString("profile");
+    if (_profile != null) {
+      try {
+        // todo 包疑问：jsonDecode
+        // profile = Profile.fromJson(jsonDecode(_profile));
+      } catch(e) {
+        print(e);
+      }
+    } else {
+      // 默认主题索引为0，代表蓝色 TODO: 语法疑问
+      profile = Profile()..theme = 0;
+    }
+
+    // 如果没有缓存策略，设置默认缓存策略 TODO: 参数疑问
+    profile.cache = profile.cache ?? CacheConfig()
+    ..enable = true
+    ..maxAge = 3600
+    ..maxCount = 100;
+
+    // 初始化网络请求相关配置 TODO: 包疑问
+    // Git.init();
+  }
+
+  // 持久化Profile信息
+  static saveProfile() =>
+      _prefs.setString("profile", jsonEncode(profile.toJson()));
+
+}
